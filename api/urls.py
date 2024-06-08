3afe83829779655b61ea22cb0f15f46de46e156b
Alexander Denisenya <alexanderdenisenya@gmail.com>
2024-06-08 11:16:11 +0300

    add create task endpoint

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,11 @@
 from django.urls import include, path, re_path
 
+from api.views import CreateTaskView
+
 app_name = 'api'
 
 urlpatterns = [
     path(r'auth/', include('djoser.urls')),
-    re_path(r'^auth/', include('djoser.urls.authtoken'))
+    re_path(r'^auth/', include('djoser.urls.authtoken')),
+    path('task/create/', CreateTaskView.as_view())
 ]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from task.serializers import CreateTaskSerializers
+
+
+class CreateTaskView(APIView):
+    permission_classes = (
+        IsAuthenticated,
+    )
+    serializer_class = CreateTaskSerializers
+
+    def post(self, request):
+        data = request.data.copy()
+        data['customer'] = request.user.id
+
+        serializer = self.serializer_class(data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
