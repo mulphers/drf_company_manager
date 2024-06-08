1ea30caf917db4ef6c81fceadc73b3b71c6a8299
Alexander Denisenya <alexanderdenisenya@gmail.com>
2024-06-08 14:36:30 +0300

    add is executor permission

--- employee/permission.py
+++ employee/permission.py
@@ -3,6 +3,11 @@ from rest_framework.permissions import BasePermission
 from employee.models import EmployeePosition
 
 
+class IsExecutor(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.position == 'EXC'
+
+
 class IsCustomer(BasePermission):
     def has_permission(self, request, view):
         return request.user.position == 'CUS'
