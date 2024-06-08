b5681d623a653d340a4159e068f3df84a1d24103
Alexander Denisenya <alexanderdenisenya@gmail.com>
2024-06-08 14:21:30 +0300

    make password write only

--- employee/serializers.py
+++ employee/serializers.py
@@ -7,3 +7,6 @@ class EmployeeSerializers(ModelSerializer):
     class Meta:
         model = Employee
         fields = '__all__'
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
