7efe6cfa8f99a3e725b1af8ae69df90a81ed93ba
Alexander Denisenya <alexanderdenisenya@gmail.com>
2024-06-08 20:29:19 +0300

    fix owner permission and add new

--- task/permission.py
+++ task/permission.py
@@ -1,8 +1,21 @@
+from django.core.exceptions import ObjectDoesNotExist
 from rest_framework.permissions import BasePermission
 
-from task.models import Task
+from task.models import Task, TaskStatus
 
 
 class IsExecutorOwner(BasePermission):
     def has_permission(self, request, view):
-        return Task.objects.get(pk=request.data.get('task_id')).executor == request.user
+        try:
+            return Task.objects.get(pk=request.data.get('task_id')).executor == request.user
+        except ObjectDoesNotExist:
+            return False
+
+
+class IsNotRealizedTask(BasePermission):
+    def has_permission(self, request, view):
+        try:
+            print(TaskStatus.realized)
+            return Task.objects.get(pk=request.data.get('task_id')).status != TaskStatus.realized
+        except ObjectDoesNotExist:
+            return False
