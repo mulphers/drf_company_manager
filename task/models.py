13e65bc704bbdad7bb3bd5087ab880ecd658eb71
Alexander Denisenya <alexanderdenisenya@gmail.com>
2024-06-08 14:05:32 +0300

    add task status to model

--- task/models.py
+++ task/models.py
@@ -1,12 +1,20 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 from base.models import ModelWithTime
 from employee.models import Employee
 
 
+class TaskStatus(models.TextChoices):
+    waiting_executor = 'WAE', _('Waiting executor')
+    in_process = 'INP', _('In process')
+    realized = 'REL', _('Realized')
+
+
 class Task(ModelWithTime):
     name = models.CharField(max_length=32)
     description = models.CharField(max_length=256)
+    status = models.CharField(max_length=3, choices=TaskStatus.choices, default=TaskStatus.waiting_executor)
     customer = models.ForeignKey(
         to=Employee,
         on_delete=models.DO_NOTHING,
